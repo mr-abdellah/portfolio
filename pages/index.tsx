1946046e734c47f5db0d5a1900853fdb3b3e172e
Abdellah Belkaid <belkaid.abdulah@gmail.com>
2022-10-09 13:46:04 +0100

    Add responsive  hero section

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,20 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
-import Header from '../components/Header/Header'
+import type { NextPage } from "next";
+import Head from "next/head";
+import Header from "../components/Header/Header";
+import Hero from "../components/Hero/Hero";
 
 const Home: NextPage = () => {
   return (
-    <div>
+    <div className="bg-[rgb(36,36,36)] text-white h-screen snap-y snap-mandatory overflow-scroll z-0 ">
       <Head>
         <title>Abdellah Belkaid | Portfolio</title>
       </Head>
       <Header />
+      <section id="hero" className="snap-center ">
+        <Hero />
+      </section>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
