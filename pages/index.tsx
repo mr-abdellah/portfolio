f37adb9be03dfbce5973bb249f1629c9e07025b9
Abdellah Belkaid <belkaid.abdulah@gmail.com>
2022-10-09 11:52:54 +0100

    add initial files

---
+++ pages/index.tsx
@@ -0,0 +1,15 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+
+const Home: NextPage = () => {
+  return (
+    <div>
+      <Head>
+        <title>Abdellah Belkaid | Portfolio</title>
+      </Head>
+      <h1 className='text-5xl text-blue-500'>heyyy</h1>
+    </div>
+  )
+}
+
+export default Home
