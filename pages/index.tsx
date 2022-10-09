85302ae59e9c8d202c86da52d873d26578a03a2e
Abdellah Belkaid <belkaid.abdulah@gmail.com>
2022-10-09 21:48:51 +0100

    add responsive projects section

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,11 @@
+/** @format */
+
 import type { NextPage } from "next";
 import Head from "next/head";
 import About from "../components/About/About";
 import Header from "../components/Header/Header";
 import Hero from "../components/Hero/Hero";
+import Projects from "../components/Projects/Projects";
 import Skills from "../components/Skills/Skills";
 
 const Home: NextPage = () => {
@@ -22,6 +25,10 @@ const Home: NextPage = () => {
 
       <section id="skills" className="snap-start ">
         <Skills />
+      </section>
+
+      <section id="project" className="snap-start ">
+        <Projects />
       </section>
     </div>
   );
